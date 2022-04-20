efe725c37669390c30d3d223bd2fe12fe1de0121
KyawKhaing(mehk) <89673231+mehkpentester@users.noreply.github.com>
2022-04-20 14:04:14 +0630

    Create fromat_string.py

---
+++ fromat_string.py
@@ -0,0 +1,7 @@
+age = 27 #this value is integer
+
+print('my age is: '+str(age))  # str() is define to string
+
+print('My age is ', age, 'my birth year is :',2022-age) 
+
+print(f'My age is {age}. My birth year is: {2022-age}')# print (f'{}) is format_string
