096bb3457840765dbc91048078c60cf099170a56
KyawKhaing(mehk) <89673231+mehkpentester@users.noreply.github.com>
2022-04-20 14:33:50 +0630

    Create Home_work_from_CCM.py

---
+++ Home_work_from_CCM.py
@@ -0,0 +1,11 @@
+username = 'admin'
+password = 'kyaw'
+
+username = input('username : ')
+password = input('password : ')
+
+if username == 'admin' and password == 'kyaw' :
+    print('Welcome admin')
+
+else :
+    print('Try again')
