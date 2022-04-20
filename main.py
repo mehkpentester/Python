2babf49d80de3bead89b26f2281cc7dc65f4b7a1
KyawKhaing(mehk) <89673231+mehkpentester@users.noreply.github.com>
2022-04-20 14:31:24 +0630

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+  print('hello ကွန်မန့် ပိတ်ထားသည် !')
+
+name=input('name :')
+
+print('hello '+name)
+
+area calculatro (PI r**2) 
+PI=3.14
+r=int(input('radius:'))  # input() is request user to type information.
+area = PI*r**2
+print(area)
