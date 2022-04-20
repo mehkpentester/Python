05f03c760526db0de6a1721b93df51411fdea050
KyawKhaing(mehk) <89673231+mehkpentester@users.noreply.github.com>
2022-04-20 13:57:55 +0630

    Create boolean.py

---
+++ boolean.py
@@ -0,0 +1,15 @@
+hungry=False
+print(hungry)
+----------------------------------
+# comparison operator  > < >= <= ==
+
+print(5<3)
+
+print(5<=5)
+
+# logical operator and or not
+
+print(5>10 and 5>13)    #{and} operator can work when both conditions are ture
+#အခြေအနေ ၂ခုလုံးမှန်မှ {and} operator က အလုပ်လုပ်မည်
+print(5>1 or 5>15)      #{or} operator can work when one conditions true
+#အခြေအနေ ၂ခုထဲမှ တစ်ခုထဲမှန်လျင်လည်း အလုပ်လုပ်သည်
