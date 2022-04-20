20d5c29beb178a2c03842e6c0db9515d35d72dae
KyawKhaing(mehk) <89673231+mehkpentester@users.noreply.github.com>
2022-04-20 14:09:17 +0630

    Create if_else.py

---
+++ if_else.py
@@ -0,0 +1,19 @@
+# age = int(input('age :'))
+
+# if age < 18 :
+#     print('you are young')
+# elif age >18 and age < 30:
+#     print('normal age')
+# else :
+#     print('you are old')
+
+tired=input('are you tired "y/n"')
+
+if tired =='y':
+    print('rest will')
+
+elif tired =='n':
+    print('go back to work')
+
+else :
+    print('Please enter correct')
