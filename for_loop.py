8f7e6d4daf4e326772c9c02d7fe3a87f113f0324
KyawKhaing(mehk) <89673231+mehkpentester@users.noreply.github.com>
2022-04-20 14:14:27 +0630

    Create for_loop.py

---
+++ for_loop.py
@@ -0,0 +1,14 @@
+ names = ['aung aung', 'kyaw kyaw', 'zaw zaw', 'su su']
+
+ for name in names:
+     if name == 'kyaw kyaw':
+         print(f'{name} is instructor')
+         break
+     else :
+         print(f'{name} is student')
+----------------------------------------------------------------
+
+fruits=['apple','orange','watermelon']
+ 
+for fruit in fruits :
+     print(f'{fruit} is fruit')
